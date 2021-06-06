f58ba0be7c4abd249b6f31cf3c29164efc945271
rachelsch1 <schwarz.rachel.e@gmail.com>
2021-06-06 12:03:14 -0400

    initial solution

--- src/main/java/ex04/base/ex04.java
+++ src/main/java/ex04/base/ex04.java
@@ -1,4 +1,26 @@
 package ex04.base;
 
+import java.util.Scanner;
+
 public class ex04 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter a noun. ");
+        String noun = input.nextLine();
+
+        System.out.print("Enter a verb. ");
+        String verb = input.nextLine();
+
+        System.out.print("Enter an adverb. ");
+        String adverb = input.nextLine();
+
+        System.out.print("Enter an adjective. ");
+        String adjective = input.nextLine();
+
+        String outputString = "Wait, you " + adverb + " " + verb + " your " + adjective + " " + noun + " too? That's neat.";
+
+        System.out.println(outputString);
+    }
+
 }
